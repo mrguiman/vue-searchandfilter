d94a540915bc71665c61c83fbe98eb108e94d0b6
Guillaume Mantopoulos <guillaume.mantopoulos@gmail.com>
2020-06-18 15:00:13 +0200

    improvements

--- src/components/SearchAndFilter.vue
+++ src/components/SearchAndFilter.vue
@@ -1,3 +1,5 @@
+<!-- Input is in a working state but scrolling can be a pain because the component is composed of
+divs and a raw input -->
 <template>
   <div class="searchandfilter">
     <div class="input-wrapper">
@@ -9,9 +11,9 @@
       <input type="text" v-model="searchKeywords" ref="rawInput" v-on:keyup="handleInputs" />
     </div>
 
-    <div v-if="availableFilters.length > 0" class="filters-dropdown">
-      <div v-for="filter in availableFilters" v-bind:key="filter" class="filter-line" v-on:click="addFilter(filter)">
-        <span>{{ filter }}:</span> <span>{{ searchKeywords }}</span>
+    <div v-if="displayedFilters.length > 0" class="filters-dropdown">
+      <div v-for="filter in displayedFilters" v-bind:key="filter" class="filter-line" v-on:click="handleFilterClick(filter)">
+        <span>{{ filter }}:</span><span>{{ searchKeywords }}</span>
       </div>
     </div>
   </div>
@@ -35,26 +37,72 @@ export default {
     availableFilters() {
       return this.filters.filter(val => {
         return this.activeFilters.map(x => x.filter).indexOf(val) === -1
-      }) 
-    }
+      })
+    },
+    displayedFilters() {
+      return this.availableFilters.filter(val => {
+        return this.searchKeywords.indexOf(`${val}:`) === -1
+      });
+    },
+    searchFilterRegex() {
+      let availableFiltersRegexSubstring = this.availableFilters.reduce((acc, curr) => {
+        if (acc.length > 0) {
+          acc += "|"
+        }
+        acc += `\\b${curr}\\b`
+        return acc;
+      }, '');
+
+      return `(${availableFiltersRegexSubstring}):([^\\s"']+ |"[^"]*"|'[^']*' )`
+    },
   },
 
   methods: {
-    addFilter(filter) {
+    handleFilterClick(filter) {
+      if (this.searchKeywords.length > 0) {
+        this.addFilter(filter, this.searchKeywords)
+      } else {
+        this.searchKeywords += `${filter}:`
+        this.$refs.rawInput.focus();
+      }
+    },
+    addFilter(filter, value) {
+      let filterValue = value || this.searchKeywords;
+
       this.activeFilters.push({
         filter: filter,
-        value: this.searchKeywords
+        value: filterValue
       });
-      this.searchKeywords = ""
+
+      // Clean up the input after adding the filter
+      this.searchKeywords = this.searchKeywords.replace(filterValue, '').trim();
+      this.searchKeywords = this.searchKeywords.replace(`${filter}:`, '').trim();
+
       this.$refs.rawInput.focus();
     },
-    handleInputs (e) {
-      if (e.key.toLowerCase() === "backspace" && this.searchKeywords.length === 0) {
-        // Pressed backspace on empty input means we get rid of the last filter
-        let lastFilter = this.activeFilters.pop();
-        if (lastFilter) {
-         this.searchKeywords = lastFilter.value;
+    popFilter() {
+          // Pressed backspace on empty input means we get rid of the last filter
+          let lastFilter = this.activeFilters.pop();
+          if (lastFilter) {
+            this.searchKeywords = `${lastFilter.filter}:${lastFilter.value}`;
+          }
+    },
+    tryMatchFilter() {
+        const filterMatch = this.searchKeywords.match(new RegExp(this.searchFilterRegex, 'g'));
+
+        if (filterMatch && filterMatch.length > 0) {
+          const [filter, value] = filterMatch[0].trim().split(':');
+          
+          this.addFilter(filter, value);
         }
+    },
+    handleInputs (e) {
+      if (this.searchKeywords.length === 0) {
+        if (e.key.toLowerCase() === "backspace") {
+          this.popFilter();
+        }
+      } else if (this.availableFilters.length > 0) {
+        this.tryMatchFilter();
       }
     }
   }
@@ -76,15 +124,32 @@ export default {
   border-radius: 5px;
   box-sizing: border-box;
   padding: 0 8px;
+  overflow: auto;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
 }
+
+.input-wrapper > * {
+  flex: 0 0 auto;
+}
+
+.input-wrapper::-webkit-scrollbar {
+  display: none
+}
+
 .input-wrapper .active-filters {
   display: inline-block;
 }
 .active-filter {
+  display:inline-block;
   background-color: #e2e2e2;
   border-radius: 8px;
   padding: 4px;
 }
+.active-filter+.active-filter {
+  margin-left: 8px;
+}
+
 .input-wrapper > input {
   display: inline-block;
   background: none;
